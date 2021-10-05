39c5b1153b6f587952450e380b56c0cc96d7caf2
dorjigyaltsen <Jolly.siluwal@gmail.com>
2021-10-05 16:18:40 +0100

    User story 1 - Table list to see pending and completed tasks.

--- ToDoItemRestfulAPIApp/wwwroot/js/site.js
+++ ToDoItemRestfulAPIApp/wwwroot/js/site.js
@@ -2,3 +2,54 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const uri = 'api/TodoItemList';
+let todos = [];
+function getToDoItems() {
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => _displayToDoItems(data))
+        .catch(error => console.error('Unable to get to-do items.', error));
+}
+
+function _displayCount(itemCount) {
+    const name = (itemCount === 1) ? 'task to-do' : 'task to-dos';
+    document.getElementById('counter').innerText = `${itemCount} ${name}`;
+}
+
+function _displayToDoItems(data) {
+    const tBody = document.getElementById('todos');
+    tBody.innerHTML = '';
+    _displayCount(data.length);
+    const button = document.createElement('button');
+    data.forEach(item => {
+        addTableRow(tBody, item, button);
+    });
+    todos = data;
+}
+
+function addTableRow(tBody, item, button) {
+    let statusLabel = document.createElement('label');
+    let status = (item.isComplete) ? 'Completed' : 'Pending';
+    statusLabel.innerHTML = status;
+
+    let editButton = button.cloneNode(false);
+    editButton.innerText = 'Edit';
+    editButton.classList.add('text-center', 'btn', 'btn-warning', 'btn-sm', 'btn-block');
+
+    let deleteButton = button.cloneNode(false);
+    deleteButton.innerText = 'Delete';
+    deleteButton.classList.add('btn', 'btn-danger', 'btn-sm', 'btn-block');
+
+    let tr = tBody.insertRow();
+    tr.classList.add('mainbody');
+    let td1 = tr.insertCell(0);
+    td1.appendChild(statusLabel);
+    let td2 = tr.insertCell(1);
+    let textNode = document.createTextNode(item.description);
+    td2.appendChild(textNode);
+    let td3 = tr.insertCell(2);
+    td3.appendChild(editButton);
+    let td4 = tr.insertCell(3);
+    td4.appendChild(deleteButton);
+}
